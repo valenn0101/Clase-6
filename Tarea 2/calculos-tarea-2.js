59cb918b0547257b3256553e7e677e73f5e65367
valenn0101 <veroncaceresvalentin@gmail.com>
2022-12-23 10:45:42 -0300

    Tarea 2 completada
    Se agrego las funciones de poder mostrar el boton calcular y los resultados.
    Tambien la funcionalidad para quitar el ultimo integrante

---
+++ Tarea 2/calculos-tarea-2.js
@@ -0,0 +1,39 @@
+function obtenerMayorSalario(numeros) {
+  let mayorNumero = numeros[0];
+  for (let i = 1; i < numeros.length; i++) {
+    if (numeros[i] > mayorNumero) {
+      mayorNumero = numeros[i];
+    }
+  }
+
+  return mayorNumero;
+}
+
+function obtenerMenorSalario(numeros) {
+  let menorNumero = numeros[0];
+  for (let i = 1; i < numeros.length; i++) {
+    if (numeros[i] < menorNumero) {
+      menorNumero = numeros[i];
+    }
+  }
+
+  return menorNumero;
+}
+
+function obtenerPromedioAnual(numeros) {
+  let acumulador = 0;
+  for (let i = 0; i < numeros.length; i++) {
+    acumulador += numeros[i];
+  }
+
+  return (acumulador / numeros.length).toFixed(2);
+}
+
+function obtenerPromedioMensual(numeros) {
+  let acumulador = 0;
+  for (let i = 0; i < numeros.length; i++) {
+    acumulador += numeros[i];
+  }
+
+  return (acumulador / 12).toFixed(2);
+}
